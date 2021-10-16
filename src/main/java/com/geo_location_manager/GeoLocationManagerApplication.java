648fc249f2a034f30367a47d7e6861698f675834
ddaGarrido <danieldagarrido@gmail.com>
2021-10-15 21:58:34 -0300

    Add project

---
+++ src/main/java/com/geo_location_manager/GeoLocationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.geo_location_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoLocationManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoLocationManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geo_location_manager/GeoLocationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geo_location_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoLocationManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
